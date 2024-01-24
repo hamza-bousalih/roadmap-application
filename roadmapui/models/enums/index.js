639fb0b51f7b9fc88d5fd91aa354a887340bf8ac
bshg <hamzabousalih3@gmail.com>
2024-01-24 16:22:35 +0100

    + fix some issues with exporting enums

--- roadmapui/models/enums/index.js
+++ roadmapui/models/enums/index.js
@@ -1,4 +1,4 @@
-export ActionStatus from "@/models/enums/ActionStatus";
-export RoadmapStatus from "@/models/enums/RoadmapStatus";
-export Role from "@/models/enums/Role"
-export TaskTypeIcon, {TaskType, TaskTypes} from "@/models/enums/TaskTypeIcon";
+// export ActionStatus from "@/models/enums/ActionStatus";
+// export RoadmapStatus from "@/models/enums/RoadmapStatus";
+// export Role from "@/models/enums/Role"
+export /* TaskTypeIcon, */ {TaskType, TaskTypes} from "@/models/enums/TaskTypeIcon";
